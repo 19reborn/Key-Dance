6efa5c4b861c8673117de1f53e7c64f2f056a66b
wr786 <wr786@pku.edu.cn>
2021-05-08 19:22:28 +0800

    refactor

--- c_demo/include/init.h
+++
@@ -1,5 +0,0 @@
-#ifndef __SNAKE_INIT__
-#define __SNAKE_INIT__
-#include "game.h"
-void InitGame(void);
-#endif
--- cxx_demo/README.md
+++
@@ -1,9 +0,0 @@
-需要将include/raylib.h和lib/libraylib.a替换成自己的
-
-测试代码：
-```shell
-make clean
-
-make
-./game
-```
