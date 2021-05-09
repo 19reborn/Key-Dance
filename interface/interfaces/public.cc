feac935b4a475142f66f47bfe2f4c32e89f09624
Fisher-Wang <kingwfsh@163.com>
2021-05-10 00:37:37 +0800

    interface changed partly

---
+++ interface/interfaces/public.cc
@@ -0,0 +1,8 @@
+#include "interface.h"
+
+Button InterfaceBase::bt_goback = Button(0., 0., screenWidth/10., screenHeight/10.);
+
+// static void draw_goback_button(InterfaceState prev_interface)
+// {
+//     DrawRectangle(0, screen)
+// }
